472ca4235b4465fa0199000dadc759546a2555f0
Tarek Jellali <jellalitarek@gmail.com>
2021-03-16 22:57:23 +0100

    add: webpack config and some imrpovements

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import './styles.scss';
+import DATA from './assets/data.json';
+
+/** Created by Tarek Jellali under CC-BY-NC licence.
+ * you can use back all this work in any other work
+ * I'd love to be credited for this work if it helped.
+ */
+ var _email = DATA.email;
+
+ function putEmails() {
+   /* Just a classic JS email protection */
+   (document.querySelector(".email-container") as HTMLDivElement).innerText = "(mailto:" + _email + ")";
+   document.querySelector(".button.mail").setAttribute("href", "mailto:" + _email);
+ }
+ putEmails();
+ 
+ function setupListners() {
+   /* mini-plus button for the name */
+   document.querySelector(".display-name").addEventListener("click", function() {
+     document.querySelector(".personal-info").classList.toggle("expanded");
+     document.querySelector(".display-name").classList.toggle("minus");
+   });
+   /* fab-button */
+   document.querySelector("button.fab-button").addEventListener("click", function() {
+     document.querySelector("nav.fab").classList.toggle("expanded");
+     document.querySelector(".fab-backdrop").classList.toggle("expanded");
+   });
+   /* email click */
+   document.querySelector(".email-container").addEventListener("click", function() {
+     window.location.href = "mailto:jellalitarek@gmail.com";
+   });
+   /* Backdrop click */
+   document.querySelector(".fab-backdrop").addEventListener("click", function() {
+     document.querySelector("nav.fab").classList.toggle("expanded");
+     document.querySelector(".fab-backdrop").classList.toggle("expanded");
+   });
+   /* Parallax for the main image background */
+   window.addEventListener("scroll", function() {
+     (document.querySelector(".col-personal") as HTMLDivElement).style.backgroundPositionY = (window.pageYOffset / 2) + "px";
+     (document.querySelector(".mobile-nav") as HTMLDivElement).style.opacity = `${((window.pageYOffset * 2) - screen.width) / screen.width}`;
+   });
+ }
+ setupListners();
+ 
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const CopywebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    app: './src/app.ts'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      }, {
+        test: /\.(png|gif|jpg|jpeg|svg|xml)$/,
+        use: [ 'url-loader' ]
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CopywebpackPlugin({
+      patterns: [
+        {
+          from: 'src/assets/**/*',
+          to: 'assets/[name][ext]',
+
+        },
+      ],
+    }),
+    new CopywebpackPlugin({
+      patterns: [
+        {
+          from: 'src/img/**/*',
+          to: 'img/[name][ext]',
+
+        },
+      ],
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  }
+};
