f6f7200b8f0429abd79023806feb3cf425eb9dbe
Tarek Jellali <jellalitarek@gmail.com>
2023-12-11 23:54:23 +0100

    chore: format and lint code
    (cherry picked from commit e14d50e0b47ea8032b3c2e0da2bbb0ac9edf7e9c)

--- src/app.ts
+++ src/app.ts
@@ -5,42 +5,61 @@ import DATA from './assets/data.json';
  * you can use back all this work in any other work
  * I'd love to be credited for this work if it helped.
  */
- const _email = DATA.email;
- const _phone = DATA.phone;
+const _email = DATA.email;
+const _phone = DATA.phone;
 
- function putEmails() {
-   /* Just a classic JS email protection */
-   (document.querySelector(".email-container") as HTMLDivElement).innerText = "(mailto:" + _email + ")";
-   (document.querySelector(".call") as HTMLDivElement).setAttribute("href", "tel:" + _phone);
-   document.querySelector(".button.mail").setAttribute("href", "mailto:" + _email);
- }
- putEmails();
- 
- function setupListners() {
-   /* mini-plus button for the name */
-   document.querySelector(".display-name").addEventListener("click", function() {
-     document.querySelector(".personal-info").classList.toggle("expanded");
-     document.querySelector(".display-name").classList.toggle("minus");
-   });
-   /* fab-button */
-   document.querySelector("button.fab-button").addEventListener("click", function() {
-     document.querySelector("nav.fab").classList.toggle("expanded");
-     document.querySelector(".fab-backdrop").classList.toggle("expanded");
-   });
-   /* email click */
-   document.querySelector(".email-container").addEventListener("click", function() {
-     window.location.href = "mailto:jellalitarek@gmail.com";
-   });
-   /* Backdrop click */
-   document.querySelector(".fab-backdrop").addEventListener("click", function() {
-     document.querySelector("nav.fab").classList.toggle("expanded");
-     document.querySelector(".fab-backdrop").classList.toggle("expanded");
-   });
-   /* Parallax for the main image background */
-   window.addEventListener("scroll", function() {
-     (document.querySelector(".col-personal") as HTMLDivElement).style.backgroundPositionY = (window.pageYOffset / 2) + "px";
-     (document.querySelector(".mobile-nav") as HTMLDivElement).style.opacity = `${((window.pageYOffset * 2) - screen.width) / screen.width}`;
-   });
- }
- setupListners();
- 
+function putEmails() {
+  /* Just a classic JS email protection */
+  (document.querySelector('.email-container') as HTMLDivElement).innerText =
+    '(mailto:' + _email + ')';
+  (document.querySelector('.call') as HTMLDivElement).setAttribute(
+    'href',
+    'tel:' + _phone
+  );
+  document
+    .querySelector('.button.mail')
+    .setAttribute('href', 'mailto:' + _email);
+}
+putEmails();
+
+function setupListners() {
+  /* mini-plus button for the name */
+  document
+    .querySelector('.display-name')
+    .addEventListener('click', function () {
+      document.querySelector('.personal-info').classList.toggle('expanded');
+      document.querySelector('.display-name').classList.toggle('minus');
+    });
+  /* fab-button */
+  document
+    .querySelector('button.fab-button')
+    .addEventListener('click', function () {
+      document.querySelector('nav.fab').classList.toggle('expanded');
+      document.querySelector('.fab-backdrop').classList.toggle('expanded');
+    });
+  /* email click */
+  document
+    .querySelector('.email-container')
+    .addEventListener('click', function () {
+      window.location.href = 'mailto:jellalitarek@gmail.com';
+    });
+  /* Backdrop click */
+  document
+    .querySelector('.fab-backdrop')
+    .addEventListener('click', function () {
+      document.querySelector('nav.fab').classList.toggle('expanded');
+      document.querySelector('.fab-backdrop').classList.toggle('expanded');
+    });
+  /* Parallax for the main image background */
+  window.addEventListener('scroll', function () {
+    (
+      document.querySelector('.col-personal') as HTMLDivElement
+    ).style.backgroundPositionY = window.pageYOffset / 2 + 'px';
+    (
+      document.querySelector('.mobile-nav') as HTMLDivElement
+    ).style.opacity = `${
+      (window.pageYOffset * 2 - screen.width) / screen.width
+    }`;
+  });
+}
+setupListners();
